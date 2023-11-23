0edf186ccf7d36b6727bc6cef6dbb04b1b648d09
근찬 <rmscks97@inu.ac.kr>
2023-11-23 20:20:34 +0900

    hw8_1

---
+++ algorithm_class/HW8/initNext.py
@@ -0,0 +1,31 @@
+def initNext(p):
+    M = len(p)
+    next[0] = -1
+    i = 0
+    j = -1
+    while i < M:
+        next[i] = j
+        while j >= 0 and p[i] != p[j]:
+            j = next[j]
+        i += 1
+        j += 1
+
+
+pattern = [
+    "aaaaaaaaa",
+    "00000001",
+    "10100111",
+    "ababca",
+    "abababca",
+    "abcabcabc",
+    "abcabcabcab",
+    "abracadabra",
+]
+for p in range(8):
+    next = [0] * 50
+    M = len(pattern[p])
+    initNext(pattern[p])
+    print(f"({p+1}) {pattern[p]}:", end=" ")
+    for i in range(1, M):
+        print(next[i], end=" ")
+    print()
