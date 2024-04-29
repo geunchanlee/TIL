d9eccb0966d76e764d36228ae26f286418194337
근찬 <rmscks97@inu.ac.kr>
2024-04-30 02:08:02 +0900

    stackCalc 괄호 검사, 식 검사

--- data_structure_class/stackCalc/stackCalc.c
+++ data_structure_class/stackCalc/stackCalc.c
@@ -2,15 +2,14 @@
 #include <string.h>
 #include <stdlib.h>
 
-// 교재의 링크드 리스트, 괄호 검사 코드를 참고했습니다
-
+// 교재의 코드들을 참고하여 작성하였습니다
 typedef struct element { // 연산자와 피연산자를 저장할 구조체입니다
     char op;     // 연산자 +,-,*,/,() 를 저장할 변수입니다
-    double num;  // 피연산자를 저장할 변수입니다
+    double num;  // 피연산자(숫자)를 저장할 변수입니다
 } element;
 
-typedef struct StackNode{           // 스택의 노드 구조체입니다 
-    element data;          // 연산자와 피연산자를 저장하는 element 구조체로 데이터를 저장합니다
+typedef struct StackNode{   // 스택의 노드 구조체입니다 
+    element data;           // 연산자와 피연산자를 저장하는 element 구조체로 데이터를 저장합니다
     struct StackNode *link; // 링크드 리스트의 다음 노드를 찾아가기 위해 자기참조 구조체로 구성합니다
 } StackNode;
 
@@ -46,7 +45,7 @@ element pop(LinkedStack *s) // 스택 맨 윗 값을 반환 함수입니다
 {
     if (is_empty(s)) // 스택이 비어있을 때는 오류 메세지를 출력하고 종료합니다
     {
-        fprintf(stderr, "스택이 비어있음\n");
+        fprintf(stderr, "Error, empty pop\n"); // 오류 발생 함수를 출력합니다
         exit(1);
     }
     else
@@ -63,17 +62,17 @@ element peek(LinkedStack *s) // 스택의 맨 위 값을 확인하는 함수입�
 {
     if (is_empty(s)) // 스택이 비었을 때는 오류 메세지를 출력하고 종료합니다
     {
-        fprintf(stderr, "스택이 비어있음\n");
+        fprintf(stderr, "Error, empty peek"); // 오류 발생 함수를 출력합니다
         exit(1);
     }
     else return s->top->data; // 스택의 맨 위 값을 반환합니다 값을 스택에서 제거하지 않고 반환만 합니다
 }
 
 
-int check_match(const char *in) // 괄호 검사 함수입니다 
+int check_match(const char *in) // 괄호의 짝이 올바른지 검사하는 함수입니다
 {
-    LinkedStack s; // 괄호 검사용 스택을 생성합니다
-    init(&s);      // 생성한 스택을 초기화합니다
+    LinkedStack s;      // 괄호 검사용 링크드 스택을 생성합니다
+    init(&s);           // 생성한 스택을 초기화합니다
     int n = strlen(in); // 문자열의 길이를 구하고
 
     for (int i = 0; i < n; i++) // 문자열의 길이만큼 반복합니다
@@ -81,7 +80,7 @@ int check_match(const char *in) // 괄호 검사 함수입니다
         if (in[i] == '(') // 반복하면서 여는 괄호일 때
         {
             element e;
-            e.op = '('; // 여는 괄호를 스택에 넣습니다
+            e.op = '(';   // 여는 괄호를 스택에 넣습니다
             push(&s, e);
         }
         else if (in[i] == ')') // 닫는 괄호일 때 스택 안에 짝이 맞는 여는 괄호가 있어야 합니다
@@ -94,10 +93,46 @@ int check_match(const char *in) // 괄호 검사 함수입니다
     return 1; // 모든 경우를 통과하면 짝이 맞기 때문에 1을 반환합니다
 }
 
+int check_expr(const char *in) // 식이 올바른지 검사하는 함수입니다
+{
+    int n = strlen(in); // 문자열의 길이를 구합니다
+    int i = 0;
+    while (i < n) {
+        if ('0' <= in[i] && in[i] <= '9') { // 숫자인 경우, 아스키코드 48(0)~57(9) 사이의 값인 경우 넘어갑니다
+            i++;
+            continue;
+        }
+        else if (in[i] == '+' || in[i] == '-' || in[i] =='.') { // 부호인 경우와 소수점인 경우
+            if (i == 0 || !('0' <= in[i-1] && in[i-1] <= '9')) 
+                return 0; // 부호가 맨 앞에 오거나 부호 앞에 숫자가 없는 경우 부호를 포함한 식이므로 0을 반환합니다
+            i++;
+            continue;
+        }
+        else if (in[i] == '*' || in[i] == '/') { // 연산자인 경우
+            if (i == 0 || !((in[i-1] >= '0' && in[i-1] <= '9') || in[i-1] == ')')) // 연산자가 맨 앞인 경우, 연산자 앞에 숫자가 없는 경우, 연산자 앞에 닫는 괄호가 없는 경우
+                return 0; // 올바르지 않은 식이므로 0을 반환합니다
+            i++;
+            continue;
+        }
+        else if (in[i] == '(' || in[i] == ')') { // 괄호인 경우
+            i++;
+            continue;
+        }
+        else return 0; // 그 외의 문자인 경우 올바르지 않은 식이므로 0을 반환합니다
+    }
+    return 1; // 모든 문자를 검사한 후에도 문제가 없으면 1을 반환합니다
+}
+
+void infix_to_postfix(char exp[]) {
+    
+}
+
 int main()
 {
-    char *str = "(3+5)*8-6/2"; // 괄호 검사를 할 문자열입니다
-    if (check_match(str)) printf("괄호 검사 성공\n"); // 괄호 검사 함수를 호출하고 성공하면 성공 메세지를 출력합니다
-    else printf("괄호 검사 실패\n"); // 실패하면 실패 메세지를 출력합니다
+    char *str = "5*(3+5.1)*8-6/2"; // 괄호 검사를 할 문자열입니다
+    if (check_match(str)) printf("%s 괄호 검사 성공\n", str); // 괄호 검사 함수를 호출하고 성공하면 성공 메세지를 출력합니다
+    else printf("%s 괄호 검사 실패\n", str); // 실패하면 실패 메세지를 출력합니다
+    if(check_expr(str)) printf("%s 식 검사 성공\n", str); // 식 검사 함수를 호출하고 성공하면 성공 메세지를 출력합니다
+    else printf("%s 식 검사 실패\n", str); // 실패하면 실패 메세지를 출력합니다
     return 0;
 } 
